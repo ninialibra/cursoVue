696516daf9118ff85ac041d87c658c721a04aecb
Tania <tanialibramg@gmail.com>
2022-01-18 22:45:12 +0100

    rutas

--- 05-rutas-ciclo/src/router/router.js
+++ 05-rutas-ciclo/src/router/router.js
@@ -33,9 +33,33 @@ const routes = [{
                 }
             },
             {
-
                 path: '',
                 redirect: { name: 'pokemon-about' }
+            }
+
+        ]
+    },
+
+    {
+        path: '/dbz',
+        name: 'dbz',
+        component: () =>
+            import ( /*webpackChunkName: "DragonBallLayout" */ '../modules/dbz/layouts/DragonBallLayout'),
+        children: [{
+                path: 'characters',
+                name: 'dbz-characters',
+                component: () =>
+                    import ( /*webpackChunkName: "Characters" */ '../modules/dbz/pages/Characters')
+            },
+            {
+                path: 'about',
+                name: 'dbz-about',
+                component: () =>
+                    import ( /*webpackChunkName: "About" */ '../modules/dbz/pages/About')
+            },
+            {
+                path: '',
+                redirect: { name: 'dbz-characters' }
             }
 
         ]
