fcbd609f374df42f98ef650252facb65daac2e5e
Tania <tanialibramg@gmail.com>
2022-06-02 22:11:20 +0200

    mutations

--- 06-bases-vuex/src/components/Counter.vue
+++ 06-bases-vuex/src/components/Counter.vue
@@ -4,6 +4,10 @@
     <h2>Direct access: {{$store.state.count}}</h2>
     <h2>Computed: {{countComputed}}</h2>
 
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button @click="incrementRandomInt" :disabled="isLoading">random</button>
+
     <h1>mapState</h1>
     <h2>mapState: {{count}}</h2>
     <h2>lastMutation: {{lastMutation}}</h2>
@@ -12,7 +16,7 @@
 
 <script>
 
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default{
 
@@ -25,8 +29,21 @@ export default{
     //...mapState(['count','lastMutation'])
     ...mapState({
       count: state=>state.count,
-      lastMutation: state=>state.lastMutation
+      lastMutation: state=>state.lastMutation,
+      isLoading: state=>state.isLoading
     })
+  },
+  methods:{
+    increment(){
+      this.$store.commit('increment')
+    },
+    incrementBy(){
+      this.$store.commit('incrementBy',5)
+    },
+    incrementRandomInt(){
+      this.$store.dispatch('incrementRandomInt');
+    },
+    ...mapActions(['incrementRandomInt'])
   }
 }
 </script>
--- 06-bases-vuex/src/store/index.js
+++ 06-bases-vuex/src/store/index.js
@@ -1,8 +1,35 @@
 import { createStore } from 'vuex';
+import getRandomInt from "../helpers/getRandomInt";
 
 export default createStore({
     state: {
         count: 1,
-        lastMutation: 'none'
+        lastMutation: 'none',
+        isLoading: false
+    },
+    mutations: { //siempre sincronas. con los commit de mutaciones se cambia el state directamente
+        increment(state) {
+            state.count++;
+            state.lastMutation = 'increment';
+        },
+        incrementBy(state, val) {
+            state.count += val;
+            state.lastMutation = 'incrementby ' + val;
+        },
+        setLoading(state, estado) {
+            state.isLoading = estado;
+        }
+    },
+    actions: { //pueden ser asincronas. la accion cambia el state con una mutacion. se llaman con dispatch
+        async incrementRandomInt({ commit }) {
+            commit('setLoading', true);
+
+            const randomInt = await getRandomInt();
+            commit('incrementBy', randomInt);
+
+            commit('setLoading', false);
+
+        }
     }
+
 })
