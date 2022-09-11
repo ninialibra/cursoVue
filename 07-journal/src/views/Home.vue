dfeb538417a1fa661cc018539d84a3c212d5959a
Tania <tanialibramg@gmail.com>
2022-09-11 23:54:13 +0200

    journal

---
+++ 07-journal/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hola mundo</h1>
+    <button class="btn btn-success">Success</button>
+   </div>
+</template>
+
