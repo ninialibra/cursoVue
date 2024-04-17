8ac8fdea17f657613929f993af2a2155d39550c9
Tania <tanialibramg@gmail.com>
2024-04-17 22:42:25 +0200

    testing

---
+++ 07-journal/tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -0,0 +1,8 @@
+import uploadImage from "@/modules/daybook/helpers/uploadImage";
+import axios from "axios";
+
+desc('pruebas en el upload image', () => {
+    test('debe cargar archivo y retornar url', async() => {
+
+    });
+});
--- 07-journal/tests/unit/modules/daybook/router/index.spec.js
+++ 07-journal/tests/unit/modules/daybook/router/index.spec.js
@@ -21,5 +21,27 @@ describe('pruebas en router module daybook', () => {
 
         expect((await daybookRouter.children[0].component()).default.name)
 
+        const promiseRoutes = [];
+        daybookRouter.children.forEach(child => promiseRoutes.push(child.component()))
+
+        const routes = (await Promise.all(promiseRoutes)).map(r => r.default.name)
+
+        expect(routes).toContain('NoEntrySelected')
+            //expect(routes).toContain('EntryView')
+
+    });
+
+    test('debe de retornar el id de la ruta', () => {
+        const route = {
+            params: {
+                id: 'ABC-123'
+            }
+        }
+
+        //expect(daybookRouter.children[1].props(route)).toEqual({ id: 'ABC-123' })
+
+        const entryRoute = daybookRouter.children.find(route => route.name === 'entry')
+        expect(entryRoute.props(route)).toEqual({ id: 'ABC-123' })
+
     });
 });
